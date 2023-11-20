72a50da2253950fbdf93a62c7329016a88c2de37
Marcos Lema <socramel@gmail.com>
2023-11-20 16:04:34 +0100

    do

--- src/base/EnteroAleatorio.java
+++ src/base/EnteroAleatorio.java
@@ -12,10 +12,6 @@ import java.util.Scanner;
  */
 
 public class EnteroAleatorio {
-	/**
-	 * @param args
-	 * Metodo para arrancar la aplicacion
-	 */
 
 	public static void main(String[] args) {
 		
@@ -30,55 +26,39 @@ public class EnteroAleatorio {
 		Scanner sc = new Scanner(System.in);
 		
 		int opcion;
-		
-		mostrarMenu();
-		
+				
 		do {			
+			System.out.println("========= Menú =========");
+			System.out.println("1. Ver el mayor de los números");
+			System.out.println("2. Obtener la suma de todos los números");
+			System.out.println("3. Salir de la aplicación");
+			System.out.println("========================");
 			System.out.println("Ingrese su opción: ");
 			opcion = sc.nextInt();
 			
 			switch (opcion) {
 			case 1:
 				int mayor = mayorNumero(numeros);
-				System.out.println("El mayor de los números es: " + mayor);
+				System.out.println("\nEl mayor de los números es: " + mayor + "\n");
 				break;
 			case 2:
 				int suma = obtenerSuma(numeros);
-				System.out.println("La suma de todos los números es: " + suma);
+				System.out.println("\nLa suma de todos los números es: " + suma + "\n");
 				break;
 			case 3:
-				System.out.println("Saliendo de la aplicación...");
+				System.out.println("\nSaliendo de la aplicación...");
 				break;
 			default:
 				System.out.println("Opción incorrecta. Por favor, teclee una opción válida.");
+				System.out.println("\n");
 				break;
 			}
-			if(opcion != 3) {
-				System.out.println("¿Desea realizar otra acción? (1. Sí, 2. No): ");
-				int continuar = sc.nextInt();
-				if (continuar == 2) {
-					opcion = 3;
-					System.out.println("Saliendo de la aplicación...");
-				}
-					}
 		} while(opcion != 3);
 		
 		sc.close();
-		
-		// System.out.println("El número entero aleatorio entre 1 y 25 es: " + numeros);
 	}
 	
-	// Mostrar menú
-	private static void mostrarMenu() {
-		
-	
-	System.out.println("Menú: ");
-	System.out.println("1. Ver el mayor de los números");
-	System.out.println("2. Obtener la suma de todos los números");
-	System.out.println("3. Salir de la aplicación");
-	}
-	
-	// Función para encontrar el número mayor en el array
+	// Método para encontrar el número mayor en el array
 	private static int mayorNumero(int[] array) {
 		int mayor = array[0];
 		for (int i = 1; i < array.length; i++) {
@@ -88,14 +68,14 @@ public class EnteroAleatorio {
 		}
 		return mayor;
 	}
-	// Función para calcular la suma de todos los números en el array
+	
+	// Método para calcular la suma de todos los números en el array
 	private static int obtenerSuma(int[]array) {
 		int suma = 0;
 		for (int i = 0; i < array.length; i++) {
 			suma += array[i];
 		}
 		return suma;
-	}
+	} 
 
 }
-
